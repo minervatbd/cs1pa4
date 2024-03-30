000d85366a0965d9f503918a8de6b081568aef60
Minerva <minervatbd>
2024-03-30 13:37:38 -0400

    add main and headers

---
+++ loyalty.c
@@ -0,0 +1,11 @@
+// COP3502C // Programming Assignment #4 // Mina Georgoudiou // 03/30/24
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[]) {
+
+}
