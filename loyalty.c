c031e81b5dc38ce88f593033c14bcc250740a4dd
Minerva <minervatbd>
2024-03-30 13:44:14 -0400

    get input file

--- loyalty.c
+++ loyalty.c
@@ -7,5 +7,21 @@
 #include <ctype.h>
 
 int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("invalid input/output args");
+        return -1;
+    }
+    else {
 
+        // open input file
+        FILE* inFile = fopen(argv[1],"r");
+
+        // make sure the file exists
+        if (inFile == NULL) {
+            printf("invalid input arg");
+            return -1;
+        }
+
+        
+    }
 }
